93fd45030d61493b83804f620eb63c0c63df05c9
Meng Lin <menglinmaker@gmail.com>
2024-11-19 00:21:49 +1100

    chore: partially port CatchBoundary - awaiting svelte error boundary implementation (#55)
    * chore: simplify typecheck test
    
    * chore: partially port CatchBoundary, awaiting Svelte error boundary implementation
    
    * test: copy .attw.json from svelte query for build type testing

---
+++ packages/svelte-router/src/CatchBoundary/CatchBoundary.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import ErrorComponent from './ErrorComponent.svelte'
+  import type { Component } from 'svelte'
+
+  type ErrorRouteComponent = Component
+
+  const props = $props<{
+    getResetKey: () => number | string
+    children: Component
+    errorComponent?: ErrorRouteComponent
+    onCatch?: (error: Error, errorInfo: any) => void
+  }>()
+
+  const RenderedErrorComponent: Component =
+    props.errorComponent ?? ErrorComponent
+</script>
+
+<!-- <svelte:boundary}> -->
+{@render props.children()}
+
+<!-- {#snippet failed(error, reset)}
+    <RenderedErrorComponent/>
+  {/snippet}
+</svelte:boundary> -->
